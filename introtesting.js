fc9d39354d4e201b3a3451155dc50cb51f34cd9d
eXiled4 <elite21ret@gmail.com>
2022-07-14 12:55:02 +1000

    comments re wriding coding test guidelines added

---
+++ introtesting.js
@@ -0,0 +1,27 @@
+// testing during the planning phase, the development phase, and at the end of development phase. 
+// To support this, you need to write testable code, code that's easily testable either via manual processes or automated tools. 
+// Absolute must for performing unit testing.
+// SOFTWARE TESTING PROCESS
+    // Test planning
+    // Test analysis
+    // Test design
+    // Construction & verification
+    // Testing cycle - usually done in a cycle, includes unit testing, system-wide testing and simple code reviews
+    // Final Testing
+    // Post Presentation
+
+// When determining requirements, perform inspection - eg if req are adequate, do they address the questions at hand
+// Document inspection done inbetween planning phases -  ege requirement planning, design, test plans
+// iN IMPLEMENTATION Apply appropriate testing tools and do code inspection, 
+// At minimium core functions must be tested
+
+// COMMON TOOLS FOR TESTING
+    // IDEs: 
+        // Web matrix, Visual Studio
+    // Open Source Testing Tools: 
+        // Selenium, Apache JMeter
+    // Windows based tools: 
+        // Test Studio
+        // CSE HTML Validator
+        // Ranorex
+
---
+++ writecodetest.js
@@ -0,0 +1,9 @@
+// Clean testable code - easy use by specific testing tools or manually
+// CHARACTERISTICS OF CLEAN TESTABLE CODE
+    // Good clean, readable structure
+    // meaningful names for functions and variables
+    // No complex functions - each function does one thing
+    // No hidden state values - not holding state in any way
+        // not trying to keep state variables in a particular global variable
+    // logical flow
+    // use of white space
